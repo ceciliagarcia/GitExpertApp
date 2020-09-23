3df3d39c8e467155af0fffa442074dd32255c696
Cecilia Garcia <ceciliagarciagon92@gmail.com>
2020-09-23 15:28:46 +0200

    Add docs

---
+++ docs/precache-manifest.5ee38326c28366d9488cba8e47b65086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cfa5ec3c6d1f4912db9ffd5391f807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0bc8e3f67f2222a883",
+    "url": "/static/css/main.e3f9e39e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0bc8e3f67f2222a883",
+    "url": "/static/js/main.7335b96f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
